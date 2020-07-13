53944b7ebc957d28fb412a4e8b32d36773928abd
Rohan Nicholls <rohan.nicholls@gmail.com>
2020-07-13 05:18:34 +0200

    ✨Data fetching, asynchronous table updates from the two api calls

--- src/App.js
+++ src/App.js
@@ -1,54 +1,35 @@
 import React, { useState, useEffect } from 'react';
 import useFetch from './hooks/useFetch';
 import './App.css';
+import Table from './components/table/table';
 
 const advertisersUrl = 'https://5b87a97d35589600143c1424.mockapi.io/api/v1/advertisers';
 const advertisersStatsUrl =
   'https://5b87a97d35589600143c1424.mockapi.io/api/v1/advertiser-statistics';
 
-function AdvertiserList({ advertisers, stats }) {
-  return (
-    <div>
-      <h1>Advertisers</h1>
-      <ul>
-        {advertisers.map((ad) => (
-          <li key={ad.id}>
-            <span>ID</span>
-            <span>{ad.id}</span>
-            <span>Name</span>
-            <span>{ad.name}</span>
-            <span>campaignIds</span>
-            <span>{ad.campaignIds.join(', ')}</span>
-          </li>
-        ))}
-      </ul>
-      <ul>
-        {stats.map((ad) => (
-          <li key={ad.advertiserId}>
-            <span>ID</span>
-            <span>{ad.advertiserId}</span>
-            <span>Impressions</span>
-            <span>{ad.impressions}</span>
-            <span>Clicks</span>
-            <span>{ad.clicks}</span>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-}
-
 function App() {
-  // const advertisers = useFetch(advertisersUrl);
-  // const stats = useFetch(advertisersStatsUrl);
   const [advertisers, setAdvertisers] = useState({ data: [], error: false, loading: true });
   const [stats, setStats] = useState({ data: [], error: false, loading: true });
 
   useFetch(advertisers, setAdvertisers, advertisersUrl);
   useFetch(stats, setStats, advertisersStatsUrl);
 
-  const error = <div>There was an error fetching the resource</div>;
-  const loading = <div>Loading...</div>;
+  const [displayData, setDisplayData] = useState(advertisers.data);
+
+  useEffect(() => {
+    setDisplayData(
+      advertisers.data.map((row) => {
+        const adStats = stats.data.find((st) => st.advertiserId === row.id);
+        return {
+          ...row,
+          impressions: adStats && adStats.impressions,
+          clicks: adStats && adStats.clicks,
+        };
+      }),
+    );
+  }, [advertisers, stats]);
+
+  const displayColumns = ['Name', 'Date Created', '# Campaigns', 'Impressions', 'Clicks'];
 
   return (
     <Table
--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -2,22 +2,21 @@ import { useEffect } from 'react';
 
 const useFetch = (getter, setter, url) => {
   useEffect(() => {
-    setTimeout(() => {
-      fetch(url)
-        .then(async (response) => {
-          if (!response.ok) {
-            console.log('request completed, but status not okay');
-            setter({ data: getter.data, error: true, loading: false });
-          } else {
-            const json = await response.json();
-            setter({ data: json, error: false, loading: false });
-          }
-        })
-        .catch((error) => {
-          console.log('We had a network error', error);
+    fetch(url)
+      .then(async (response) => {
+        if (!response.ok) {
+          console.log('request completed, but status not okay');
           setter({ data: getter.data, error: true, loading: false });
-        });
-    }, 2000);
+        } else {
+          const json = await response.json();
+          console.table(json);
+          setter({ data: json, error: false, loading: false });
+        }
+      })
+      .catch((error) => {
+        console.log('We had a network error', error);
+        setter({ data: getter.data, error: true, loading: false });
+      });
   }, []);
 };
 
