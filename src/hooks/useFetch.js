da1503143d26d7ca828990ac3d4245ba4e099307
Rohan Nicholls <rohan.nicholls@gmail.com>
2020-07-13 10:16:26 +0200

    🔥  remove logging

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -5,16 +5,13 @@ const useFetch = (getter, setter, url) => {
     fetch(url)
       .then(async (response) => {
         if (!response.ok) {
-          console.log('request completed, but status not okay');
           setter({ data: getter.data, error: true, loading: false });
         } else {
           const json = await response.json();
-          console.table(json);
           setter({ data: json, error: false, loading: false });
         }
       })
       .catch((error) => {
-        console.log('We had a network error', error);
         setter({ data: getter.data, error: true, loading: false });
       });
   }, []);
